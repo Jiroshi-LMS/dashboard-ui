974b752c9fe2cc7ce938bc36089280145e70f876
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-01-04 01:47:31 +0530

    Courses Page Title Update

--- app/(routes)/instructor/dashboard/courses/page.tsx
+++ app/(routes)/instructor/dashboard/courses/page.tsx
@@ -4,7 +4,7 @@ import CourseListData from "@/feature/courses/components/courses/CourseListData"
 const courseManagementPage = () => {
   return (
     <main className="main-container">
-      <h1 className="page-title">All your courses</h1>
+      <h1 className="page-title">All Your Courses</h1>
       <CourseListData />
     </main>
   );
