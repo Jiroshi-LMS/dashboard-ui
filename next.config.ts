51822ec1d419fc65be7cae6e6081d564a89db8b2
Muteen Kundangar <kundangarmuteen@gmail.com>
2026-03-03 19:18:17 +0530

    Updated next config to prevent clickjacking on the platform

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,32 @@
-import { hostname } from "os";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "github.com",
-      },
-      {
-        protocol: "https",
-        hostname: "img.freepik.com"
-      },
-      {
-        protocol: "https",
-        hostname: "jiroshi-static-dev.s3.ap-south-1.amazonaws.com"
-      },
-      {
-        protocol: "https",
-        hostname: "jiroshi-media-dev.s3.ap-south-1.amazonaws.com"
-      },
-      {
-        protocol: "https",
-        hostname: "jiroshi-static-prod.s3.ap-south-1.amazonaws.com"
-      },
-      {
-        protocol: "https",
-        hostname: "jiroshi-media-prod.s3.ap-south-1.amazonaws.com"
-      }
+      { protocol: "https", hostname: "github.com" },
+      { protocol: "https", hostname: "img.freepik.com" },
+      { protocol: "https", hostname: "jiroshi-static-dev.s3.ap-south-1.amazonaws.com" },
+      { protocol: "https", hostname: "jiroshi-media-dev.s3.ap-south-1.amazonaws.com" },
+      { protocol: "https", hostname: "jiroshi-static-prod.s3.ap-south-1.amazonaws.com" },
+      { protocol: "https", hostname: "jiroshi-media-prod.s3.ap-south-1.amazonaws.com" }
     ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'none';",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
   },
 };
 
